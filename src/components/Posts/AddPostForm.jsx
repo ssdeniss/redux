73a732b9483cabe45321d7ac19017a2cdc98c2ea
Denis <69592277+ssdeniss@users.noreply.github.com>
2022-07-05 18:32:32 +0300

    .

--- src/components/Posts/AddPostForm.jsx
+++ src/components/Posts/AddPostForm.jsx
@@ -1,23 +1,32 @@
 import React, { useState } from 'react'
-import { useDispatch } from 'react-redux'
-import { nanoid } from '@reduxjs/toolkit'
+import { useDispatch, useSelector } from 'react-redux'
 import { postAdded } from '../../features/posts/postsSlice'
+import { selectAllUsers } from '../../features/users/usersSlice'
 
 const AddPostForm = () => {
     const [title, setTitle] = useState('')
     const [content, setContent] = useState('')
+    const [userId, setUserId] = useState('')
+    const users = useSelector(selectAllUsers)
     const dispatch = useDispatch()
     const onTitleChanged = e => setTitle(e.target.value)
     const onContentChanged = e => setContent(e.target.value)
+    const onAuthorChanged = e => setUserId(e.target.value)
     const onSavePostClicked = () => {
         if (title && content) {
             dispatch(
-                postAdded(title, content)
+                postAdded(title, content, userId)
             )
             setTitle('')
             setContent('')
         }
     }
+    const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
+    const usersOptions = users.map(user => (
+        <option key={user.id} value={user.id}>
+            {user.name}
+        </option>
+    ))
 
     return (
         <section className='container form'>
@@ -26,14 +35,16 @@ const AddPostForm = () => {
                     <label for="name">Title: </label>
                     <input type="text" value={title} onChange={onTitleChanged} name="user_name" />
                 </div>
-
+                <select value={userId} onChange={onAuthorChanged}>
+                    {usersOptions}
+                </select>
                 <div>
                     <label for="msg">Content: </label>
                     <textarea value={content} onChange={onContentChanged} name="user_message"></textarea>
                 </div>
 
                 <div class="button">
-                    <button type='button' onClick={onSavePostClicked}>Send your post</button>
+                    <button type='button' onClick={onSavePostClicked} disabled={!canSave}>Send your post</button>
                 </div>
             </form>
         </section>
--- src/components/Posts/PostsList.jsx
+++ src/components/Posts/PostsList.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { selectAllPosts } from '../../features/posts/postsSlice'
+import PostAuthor from './PostAuthor'
 
 const PostsList = () => {
     const posts = useSelector(selectAllPosts)
     const renderPosts = posts.map(post => (
         <article key={post.id}>
             <h3>{post.title}</h3>
+            <PostAuthor userId={post.userId} />
             <p>{post.content.substring(0, 100)}</p>
         </article>
     ))
